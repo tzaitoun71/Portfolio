c44234bebf54d549c92af86c54cf42e2e3c39545
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2024-07-22 22:38:55 -0400

    Initial Commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".multiple-text", {
+	strings: ["New Graduate","Software Engineer","Machine Learning Engineer","Dreamer" ],
+	typeSpeed: 100,
+	backSpeed: 100,
+	backDelay: 1000,
+	loop: true
+})
