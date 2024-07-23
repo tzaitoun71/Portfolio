16bad9600a79e85ab802965c6beb035a98be1121
Tariq Zaitoun <tariq.s.zaitoun@gmail.com>
2024-07-23 18:38:32 -0400

    Added Home, Projects, and Skills sections

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".multiple-text", {
-	strings: ["New Graduate","Software Engineer","Machine Learning Engineer","Dreamer" ],
+	strings: ["New Graduate","Software Engineer","Machine Learning Engineer","Creator"],
 	typeSpeed: 100,
 	backSpeed: 100,
 	backDelay: 1000,
